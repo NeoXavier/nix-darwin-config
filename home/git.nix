d9086932049209eb7763482ff5d2328bbd51756b
NeoXavier <xavierneo88@gmail.com>
2024-09-22 23:19:06 +0800

    Added github signing keys

--- home/git.nix
+++ home/git.nix
@@ -1,4 +1,4 @@
-{...}: {
+{ ... }: {
   programs.git = {
     enable = true;
     aliases = {
@@ -11,23 +11,30 @@
     userName = "NeoXavier";
     userEmail = "xavierneo88@gmail.com";
     signing = {
-      key = "7E3F503DB9A7E8B3";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvthpQx+4xQBZ2HenJ/ubjLhmr68Dw0+00k+Ot+xj9J";
       signByDefault = true;
     };
     # aliases = {
-    #   cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
+    #   cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d
     #   prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
     #   root = "rev-parse --show-toplevel";
     # };
     extraConfig = {
       branch.autosetuprebase = "always";
       color.ui = true;
-      # core.askPass = ""; # needs to be empty to use terminal for ask pass
-      # credential.helper = "store"; # want to make this more secure
       github.user = "NeoXavier";
-      push.default = "tracking";
+      push = {
+        default = "tracking";
+        autoSetupRemote = true;
+
+      };
       init.defaultBranch = "master";
+      commit.gpsign = true;
+      gpg = {
+        format = "ssh";
+        ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+      };
     };
-    ignores = [".direnv"];
+    ignores = [ ".direnv" ];
   };
 }
